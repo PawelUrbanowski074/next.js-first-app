c0cd31a5252eac5ad4e2546732f8125c35161851
palel <32599663+palel@users.noreply.github.com>
2022-12-14 23:17:35 +0100

    MeetupDetail page with component  created

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Wawel_hill_%28view_from_W-winter%29%2C_Old_Town%2C_Krak%C3%B3w%2C_Poland.jpg/1024px-Wawel_hill_%28view_from_W-winter%29%2C_Old_Town%2C_Krak%C3%B3w%2C_Poland.jpg"
+      title="A first meetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
