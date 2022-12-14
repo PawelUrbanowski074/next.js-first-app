3ec8ed235352ed40a9ee14755cd0a39bef56e6da
palel <32599663+palel@users.noreply.github.com>
2022-12-14 22:49:26 +0100

    Layout added to main component of app

--- pages/index.js
+++ pages/index.js
@@ -16,12 +16,12 @@ const Dummy_Meetups = [
     description: 'This is a first meetup!'
   },
   {
-    id: 'm2',
+    id: 'm3',
     title: 'A third meetup',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Wawel_hill_%28view_from_W-winter%29%2C_Old_Town%2C_Krak%C3%B3w%2C_Poland.jpg/1024px-Wawel_hill_%28view_from_W-winter%29%2C_Old_Town%2C_Krak%C3%B3w%2C_Poland.jpg',
     address: 'Some address 6, 12345 Some City',
     description: 'This is a first meetup!'
-  }
+  },
 ];
 
 function HomePage() {
