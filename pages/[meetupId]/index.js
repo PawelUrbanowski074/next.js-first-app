746bd3ac65dfbf83f27710238b0211da1962114e
palel <32599663+palel@users.noreply.github.com>
2023-01-04 15:26:54 +0100

    using getStaticProps function to fetch data cd

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,42 @@ function MeetupDetails() {
   );
 };
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        }
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        }
+      }
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+
+  const meetupId = context.params.meetupId; 
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Wawel_hill_%28view_from_W-winter%29%2C_Old_Town%2C_Krak%C3%B3w%2C_Poland.jpg/1024px-Wawel_hill_%28view_from_W-winter%29%2C_Old_Town%2C_Krak%C3%B3w%2C_Poland.jpg",
+        id: meetupId,
+        title: "A first meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a first meetup",
+      },
+    },
+  }
+}
+
 export default MeetupDetails;
