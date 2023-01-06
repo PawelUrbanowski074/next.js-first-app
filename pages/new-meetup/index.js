8752b3d17c5a25d2dd6db55d1e657f8efe8b5199
palel <32599663+palel@users.noreply.github.com>
2023-01-06 15:00:35 +0100

    sending data to api

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,11 +1,36 @@
-import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import Head from "next/head";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
-function NewMeetupPage () {
-  function addMeetupHandler(enteredMeetupData) {
-    console.log(enteredMeetupData);
+function NewMeetupPage() {
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    router.push("/");
   }
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a new Meetup</title>
+        <meta
+          name="description"
+          content="Add your oew meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
 }
 
 export default NewMeetupPage;
